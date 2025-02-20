212ba6ff7f302b1b9c4eb8b00685ff38448ae660
Kiplangat Korir <korirkiplangat22@gmail.com>
2025-02-20 12:13:04 +0300

    Backend Setup

--- vite.config.ts
+++ vite.config.ts
@@ -7,20 +7,14 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
     themePlugin(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
   ],
+  base: "/korir.github.io/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
@@ -29,7 +23,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
 });
